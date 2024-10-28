7c2c3bc6fe670009be541476f5dedcd0a4f529e3
carib4ik <83468679+carib4ik@users.noreply.github.com>
2024-10-28 12:19:35 +0800

    Initial

---
+++ Assets/Scripts/Game/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Game
+{
+    public class GameController : MonoBehaviour
+    {
+        [SerializeField]
+        private float _sceneChangeDelay;
+
+        private void Awake()
+        {
+            //максимальное значение кадров в секунду
+            Application.targetFrameRate = 60;
+        }
+
+        //вызывается по ивенту смерти игрока
+        [UsedImplicitly]
+        public void OnPlayerDied() 
+        {
+            StartCoroutine(ShowGameOver());
+        }
+
+        //задержка, чтобы успели проиграться анимация и звук смерти игрока.
+        private IEnumerator ShowGameOver()
+        {
+            yield return new WaitForSeconds(_sceneChangeDelay); 
+            SceneManager.LoadSceneAsync(GlobalConstants.GAME_OVER_SCENE);
+        }
+    }
+}
